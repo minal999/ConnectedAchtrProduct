d78e1a9223e7115c73c8c6131e7547ab49a4f44a
minal999 <minughadge555@gmail.com>
2022-11-04 17:36:55 +0530

    Add project files.

---
+++ ConnectedAchtrProduct/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ConnectedAchtrProduct
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            conn = new SqlConnection(constr);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "Insert into product values(@Name,@Price)";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Price", Convert.ToDecimal(txtPrice.Text));
+                conn.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record Inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "Update product set Name=@Name,Price=@Price where Id=@Id";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(txtID.Text));
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Price", Convert.ToInt32(txtPrice.Text));
+                conn.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record Updated");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                string query = "Delete from product where Id=@Id";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(txtID.Text));
+                conn.Open();
+                int result=cmd.ExecuteNonQuery();
+                if(result==1)
+                {
+                    MessageBox.Show("Record Deleted");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from product where Id=@Id";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(txtID.Text));
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtName.Text = reader["Name"].ToString();
+                        txtPrice.Text = reader["Price"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record Not Found");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from product";
+                cmd = new SqlCommand(query,conn);
+                conn.Open();
+                reader = cmd.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataGridView1.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("Record Not Found");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
